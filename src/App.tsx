990ac5e0e84d64e1434c8d7f4a72d7d832c8e153
LiviaVCzechi <129322332+LiviaVCzechi@users.noreply.github.com>
2025-05-17 12:40:26 +0200

    Restore routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import TimelinePage from './pages/TimelinePage';
+import ExhibitionsPage from './pages/ExhibitionsPage';
+import DocumentsPage from './pages/DocumentsPage';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +13,9 @@ const App: React.FC = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="timeline" element={<TimelinePage />} />
+        <Route path="exhibitions" element={<ExhibitionsPage />} />
+        <Route path="documents" element={<DocumentsPage />} />
       </Route>
     </Routes>
   );
